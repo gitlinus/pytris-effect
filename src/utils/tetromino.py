98d4e8007a8c7e637d5b67b7dc8edf49b21323c4
gitlinus <linus.j.chen@gmail.com>
2021-06-16 13:48:57 -0400

    Added tetromino class

---
+++ src/utils/tetromino.py
@@ -0,0 +1,56 @@
+import random
+
+class Tetromino:
+
+	# controls piece generation
+	tetromino2rgb = {'I':(0,255,255),'J':(0,0,255),'L':(255,128,0),'O':(255,255,0),'S':(0,255,0),'T':(255,0,255),'Z':(255,0,0)}
+	tetrominos = ['I','J','L','O','S','T','Z']
+
+	def __init__(self,bagged=True):
+		self.hold = ""
+		self.current_tetromino = ""
+		self.queue = []
+		self.bagged = False
+		if bagged: # 7-bag system by default
+			self.bagged = True
+		self.initQueue()
+
+	def initQueue(self): # start with two bags generated
+		if self.bagged:
+			self.genNextBag()
+			self.genNextBag()	
+		elif not self.bagged:
+			self.genRandomTetromino()
+
+	def genNextBag(self): # 7-bag system
+		random.shuffle(self.tetrominos)
+		self.queue += self.tetrominos
+
+	def genRandomTetromino(self): # pseudo-random system
+		idx = random.randint(0,6)
+		self.queue.append(self.tetrominos[idx])
+
+	def nextTetromino(self):
+		self.current_tetromino = self.queue.pop(0)
+		if len(self.queue)<=7 and self.bagged: #
+			self.genNextBag()
+		elif not self.bagged:
+			self.genRandomTetromino()
+
+	def swapHold(self):
+		if self.hold=="":
+			self.hold = self.current_tetromino
+			self.nextTetromino()
+		else:
+			temp = self.hold
+			self.hold = self.current_tetromino
+			self.current_tetromino = temp
+
+	def getQueue(self):
+		return self.queue
+	
+	def getHold(self):
+		return self.hold
+
+	def getCurrentTetromino(self):
+		return self.current_tetromino		
