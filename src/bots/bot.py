683e1ec234f680da566cab16776532271b568045
gitlinus <linus.j.chen@gmail.com>
2022-12-28 05:04:20 -0500

    Trying basic tsd masking

--- src/bots/bot.py
+++ src/bots/bot.py
@@ -66,6 +66,25 @@ good_clears = [
     "PERFECT CLEAR",
 ]
 
+# 0 = empty or filled (no weighting), -1 = must be empty (negative score if filled), 1 = must be filled (positive score if filled)
+# 2 = if any of the surrounding mask cells which are >=1 are filled, then itself must also be filled
+# 3 = extra weight if filled
+# right-overhang and left-overhang separately for now
+
+ro_tsd_mask = np.array([
+    [ 0,-1,-1, 3, 0],
+    [ 1,-1,-1,-1, 1],
+    [ 1, 2,-1, 2, 1],
+], dtype = int)
+
+lo_tsd_mask = np.array([
+    [ 0, 3,-1,-1, 0],
+    [ 1,-1,-1,-1, 1],
+    [ 1, 2,-1, 2, 1],
+], dtype = int)
+
+mask_list = [ro_tsd_mask, lo_tsd_mask]
+
 class State:
         
     def __init__(self, board, hold=None, cur=None, move_score=0, lines_cleared=0, clear_type=[]):
@@ -101,6 +120,80 @@ class HeuristicBot(Bot):
     def __init__(self):
         super().__init__()
 
+    def mask_score(self, state : State): # favour filling a mask
+        m = state.board
+        max_mask_score = 0
+        for idx, mask in enumerate(mask_list):
+            for j in range(10):
+                for i in range(20):
+                    cur_mask_score = 0
+                    extra_weight = 0
+                    if i+2+mask.shape[0]-1 < 22 and j+mask.shape[1]-1 < 10:
+                        # "convolution"
+                        valid = True
+                        for mask_i in range(mask.shape[0]):
+                            for mask_j in range(mask.shape[1]):
+                                if (i+2+mask_i, j+mask_j) in state.cur:
+                                    valid = False
+                                    break
+                                else:
+                                    if(m[i+2+mask_i][j+mask_j] * mask[mask_i][mask_j] < 0):
+                                        valid = False
+                                        break
+                                    elif(m[i+2+mask_i][j+mask_j] * mask[mask_i][mask_j] > 0):
+                                        cur_mask_score += 1
+                                        if(mask[mask_i][mask_j] == 3):
+                                            extra_weight += 10
+                                    else:
+                                        if(mask[mask_i][mask_j] == 2):
+                                            for di in [-1,0,1]:
+                                                for dj in [-1,0,1]:
+                                                    if(di == 0 and dj == 0):
+                                                        continue
+                                                    if( 0 <= mask_i + di and mask_i + di < mask.shape[0] and
+                                                        0 <= mask_j + dj and mask_j + dj < mask.shape[1]):
+                                                        if(mask[mask_i+di][mask_j+dj] >= 1 and m[i+2+mask_i+di][j+mask_j+dj] != 0):
+                                                            valid = valid and m[i+2+mask_i][j+mask_j] != 0 # this cell must be filled in order to be valid
+                                                            if not valid:
+                                                                break
+                                                if not valid:
+                                                    break
+                                            if not valid:
+                                                break
+                            if not valid:
+                                cur_mask_score = 0
+                                break
+                        if valid and cur_mask_score > 0:
+                            # print(f"mask {idx} at {i+2},{j} with current score {cur_mask_score}")
+                            mask_match=(mask>0).sum()
+                            if(cur_mask_score == mask_match):
+                                # extend mask to the left and right
+                                for ii in range(mask.shape[0]):
+                                    if(j==0):
+                                        cur_mask_score += mask[ii][0] if m[i+2+ii][j]==0 and mask[ii][0]>=1 else 2*mask[ii][0]
+                                        mask_match += 2*mask[ii][0]
+                                    else:
+                                        for jj in range(j):
+                                            cur_mask_score += mask[ii][0] if m[i+2+ii][jj]==0 and mask[ii][0]>=1 else 2*mask[ii][0]
+                                            mask_match += 2*mask[ii][0]
+                                    if(j+mask.shape[1]-1==9):
+                                        cur_mask_score += mask[ii][mask.shape[1]-1] if m[i+2+ii][j+mask.shape[1]-1]==0 and mask[ii][mask.shape[1]-1]>=1 else 2*mask[ii][mask.shape[1]-1]
+                                        mask_match += 2*mask[ii][mask.shape[1]-1]
+                                    else:
+                                        for jj in range(9 - (j+mask.shape[1]-1)):
+                                            cur_mask_score += mask[ii][mask.shape[1]-1] if m[i+2+ii][j+mask.shape[1]-1+jj+1]==0 and mask[ii][mask.shape[1]-1]>=1 else 2*mask[ii][mask.shape[1]-1]
+                                            mask_match += 2*mask[ii][mask.shape[1]-1]
+                                # print(f"prefinal score of {cur_mask_score}")
+                                # how close are we to full mask match
+                                cur_mask_score = int(cur_mask_score**2 * cur_mask_score / mask_match + 1) + extra_weight**2
+                            else:
+                                if(cur_mask_score > mask_match):
+                                    assert False, "WTF"
+                                cur_mask_score = cur_mask_score**3
+                            # print(f"final score of {cur_mask_score}")
+                    max_mask_score = max(max_mask_score,cur_mask_score)
+        return max_mask_score
+
     def jaggedness_score(self, state : State): # favour keeping stack flat and avoid making islands
         m = state.board
         sm = 0
@@ -134,7 +227,7 @@ class HeuristicBot(Bot):
         if(state.lines_cleared == 0):
             return 0
         elif(any(x in state.clear_type for x in good_clears)):
-            return 2*state.move_score
+            return state.move_score**2
         else:
             return -state.lines_cleared*200
 
@@ -151,7 +244,7 @@ class HeuristicBot(Bot):
         return -sm
 
     def score(self, state : State):
-        return self.jaggedness_score(state) + self.clear_score(state) + self.height_score(state)
+        return self.jaggedness_score(state) + self.clear_score(state) + self.height_score(state) + self.mask_score(state)
             
     @profile
     def tree_search(self, gamestate):
